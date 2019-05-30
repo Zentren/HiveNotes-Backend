a5c83b130baf1d07553c7beb7f818684ef393893
Rowan Reeve <rowan@bbd.co.za>
2019-05-30 14:07:27 +0200

    Added module IDs to api/hive/courses endpoint response.

--- helpers/hive-db.js
+++ helpers/hive-db.js
@@ -7,3 +7,16 @@ export async function selectAllCourses() {
     .query('SELECT * FROM dbo.Courses');
   return result.recordset;
 }
+
+export async function selectCourseSections(courseId) {
+  const pool = await poolPromise;
+  const result = await pool.request()
+    .query(
+      `SELECT dbo.CourseSections.SectionId, Code, Name, Description, PathToMarkdown
+      FROM dbo.CourseSections
+      JOIN dbo.Sections
+      ON dbo.CourseSections.SectionId=dbo.Sections.SectionId
+      WHERE CourseId=${courseId}`
+    );
+  return result.recordset;
+}
--- routes/hive.js
+++ routes/hive.js
@@ -1,11 +1,24 @@
 import express from 'express';
-import { selectAllCourses } from '../helpers/hive-db';
+import { selectAllCourses, selectCourseSections } from '../helpers/hive-db';
 
 
 const router = express.Router();
 
 router.get('/courses', async (req, res) => {
-  res.json(await selectAllCourses());
+  const courses = await selectAllCourses();
+  let result = [];
+  for (const course of courses) {
+    const courseSections = await selectCourseSections(course.CourseId);
+    result.push({
+      courseId: course.CourseId,
+      code: course.Code,
+      name: course.Name,
+      description: course.Description,
+      icon: course.Icon,
+      modules: courseSections.map(courseSection => courseSection.SectionId)
+    });
+  };
+  res.json(result);
 });
 
 export default router;
