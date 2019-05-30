0075bbdb70f04b0755f48dae1a519fa8ff2136c3
Rowan Reeve <rowan@bbd.co.za>
2019-05-30 17:09:12 +0200

    Added singular course, and modules db queries and rest endpoints.

--- helpers/hive-db.js
+++ helpers/hive-db.js
@@ -1,10 +1,23 @@
 import { poolPromise } from '../db';
 
+export async function selectCourse(courseId) {
+  const pool = await poolPromise;
+  const result = await pool.request()
+    .query(`SELECT * FROM dbo.Courses WHERE CourseId=${courseId}`);
+  return result.recordset;
+}
 
 export async function selectAllCourses() {
   const pool = await poolPromise;
   const result = await pool.request()
     .query('SELECT * FROM dbo.Courses');
+  return result.recordset;
+}
+
+export async function selectSection(sectionId) {
+  const pool = await poolPromise;
+  const result = await pool.request()
+    .query(`SELECT * FROM dbo.Sections WHERE sectionId=${sectionId}`);
   return result.recordset;
 }
 
--- routes/hive.js
+++ routes/hive.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { selectAllCourses, selectCourseSections } from '../helpers/hive-db';
+import { selectAllCourses, selectCourseSections, selectCourse } from '../helpers/hive-db';
 
 
 const router = express.Router();
@@ -21,4 +21,30 @@ router.get('/courses', async (req, res) => {
   res.json(result);
 });
 
+router.get('/course/:courseId', async (req, res) => {
+  const course = (await selectCourse(req.params.courseId))[0];
+  const courseSections = await selectCourseSections(course.CourseId);
+  let result = {
+    courseId: course.CourseId,
+    code: course.Code,
+    name: course.Name,
+    description: course.Description,
+    icon: course.Icon,
+    modules: courseSections.map(courseSection => courseSection.SectionId)
+  };
+  res.json(result);
+});
+
 export default router;
+
+router.get('/course/:courseId/modules/', async (req, res) => {
+  const sections = await selectCourseSections(req.params.courseId);
+  let result = [];
+  for (const section of sections) {
+    result.push({
+      id: section.SectionId,
+      name: section.Name
+    });
+  }
+  res.json(result);
+});
