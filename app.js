a33be895063201f875f7b408d7b2401a27b61ea6
Rowan Reeve <rowan@bbd.co.za>
2019-05-30 10:28:41 +0200

    Added CORS for REST.

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import cors from 'cors';
+
 import hiveRouter from './routes/hive';
 
 
 const app = express();
+
+app.use(cors());
 
 app.use('/api/hive', hiveRouter);
 
